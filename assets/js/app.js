c6f751c0412bb7eab9fff46ac07af959e86934fe
rubel1997 <rubelmjhr@gmail.com>
2022-09-14 23:07:42 +0600

    added search

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,6 +10,16 @@
             $(".offcanvas-header").toggleClass("active");
         });
 
+        /*-------------------------------------------------------------------------------
+        javaScript search header
+        -------------------------------------------------------------------------------*/
+        $(".search-triger").on("click", function() {
+            $(".search-bar").addClass("active");
+        });
+        $(".search-close").on("click", function() {
+            $(".search-bar").removeClass("active");
+        });
+
         /*---------------------------------
         Javascript for AOS animation
         ----------------------------------*/
